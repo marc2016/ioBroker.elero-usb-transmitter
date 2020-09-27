34a708a664e07e266c2765662da5ce041969fe89
Marc Lammers <marc@lammers.dev>
2020-09-27 22:46:07 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'elero-usb-transmitter adapter settings': {
+        en: 'Adapter settings for elero-usb-transmitter',
+        de: 'Adaptereinstellungen für elero-usb-transmitter',
+        ru: 'Настройки адаптера для elero-usb-transmitter',
+        pt: 'Configurações do adaptador para elero-usb-transmitter',
+        nl: 'Adapterinstellingen voor elero-usb-transmitter',
+        fr: "Paramètres d'adaptateur pour elero-usb-transmitter",
+        it: "Impostazioni dell'adattatore per elero-usb-transmitter",
+        es: 'Ajustes del adaptador para elero-usb-transmitter',
+        pl: 'Ustawienia adaptera dla elero-usb-transmitter',
+        'zh-cn': 'elero-usb-transmitter的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'option1',
+        ru: 'option1',
+        pt: 'option1',
+        nl: 'option1',
+        fr: 'option1',
+        it: 'option1',
+        es: 'option1',
+        pl: 'option1',
+        'zh-cn': 'option1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'option2',
+        ru: 'option2',
+        pt: 'option2',
+        nl: 'option2',
+        fr: 'option2',
+        it: 'option2',
+        es: 'option2',
+        pl: 'option2',
+        'zh-cn': 'option2',
+    },
+};
