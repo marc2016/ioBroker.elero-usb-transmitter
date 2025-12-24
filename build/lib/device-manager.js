d7a013a1581783bd406b390481a096403de4931d
Marc Lammers <marc@lammers.dev>
2025-12-24 07:55:46 +0100

    chore: update build artifacts

--- build/lib/device-manager.js
+++ build/lib/device-manager.js
@@ -31,38 +31,86 @@ class DeviceManager {
                 activeChannels = yield this.client.checkChannels();
             }
             this.adapter.log.debug('Iterate over active channels and create devices.');
-            activeChannels.forEach((element) => {
-                this.adapter.log.info(`Active channel: ${element}`);
-                this.createEleroDevice(element);
-            });
+            for (const channel of activeChannels) {
+                this.adapter.log.info(`Active channel: ${channel}`);
+                yield this.createEleroDevice(channel);
+            }
         });
     }
     createEleroDevice(channel) {
-        this.adapter.log.debug(`Create device with channel ${channel}.`);
-        // create device with channel number as ID.
-        this.adapter.createDevice(`channel_${channel.toString()}`);
-        this.adapter.log.debug(`Create state channel.`);
-        this.adapter.createState(`channel_${channel.toString()}`, '', 'channel', { role: 'text', write: false, def: channel, defAck: true, type: 'number' }, undefined);
-        this.adapter.log.debug(`Create state controlCommand.`);
-        this.adapter.createState(`channel_${channel.toString()}`, '', 'controlCommand', {
-            role: 'state',
-            states: {
-                16: elero_usb_transmitter_client_1.ControlCommand[16],
-                32: elero_usb_transmitter_client_1.ControlCommand[32],
-                36: elero_usb_transmitter_client_1.ControlCommand[36],
-                64: elero_usb_transmitter_client_1.ControlCommand[64],
-                68: elero_usb_transmitter_client_1.ControlCommand[68],
-            },
-            write: true,
-            def: 16,
-            defAck: true,
-            type: 'number',
-        }, undefined);
-        this.adapter.log.debug(`Create state info.`);
-        this.adapter.createState(`channel_${channel.toString()}`, '', 'info', { role: 'text', write: false, def: '', type: 'string' }, undefined);
-        this.adapter.log.debug(`Create state open.`);
-        this.adapter.createState(`channel_${channel.toString()}`, '', 'open', { role: 'switch', read: true, write: true, def: false, type: 'boolean' }, undefined);
-        this.adapter.log.debug(`Device with channel ${channel} created.`);
+        return __awaiter(this, void 0, void 0, function* () {
+            this.adapter.log.debug(`Create device with channel ${channel}.`);
+            // create device with channel number as ID.
+            yield this.adapter.setObjectNotExistsAsync(`channel_${channel}`, {
+                type: 'device',
+                common: {
+                    name: `channel_${channel}`,
+                },
+                native: {},
+            });
+            this.adapter.log.debug(`Create state channel.`);
+            yield this.adapter.setObjectNotExistsAsync(`channel_${channel}.channel`, {
+                type: 'state',
+                common: {
+                    name: 'Channel',
+                    role: 'text',
+                    write: false,
+                    def: channel,
+                    defAck: true,
+                    type: 'number',
+                    read: true,
+                },
+                native: {},
+            });
+            this.adapter.log.debug(`Create state controlCommand.`);
+            yield this.adapter.setObjectNotExistsAsync(`channel_${channel}.controlCommand`, {
+                type: 'state',
+                common: {
+                    name: 'Control Command',
+                    role: 'state',
+                    states: {
+                        16: elero_usb_transmitter_client_1.ControlCommand[16],
+                        32: elero_usb_transmitter_client_1.ControlCommand[32],
+                        36: elero_usb_transmitter_client_1.ControlCommand[36],
+                        64: elero_usb_transmitter_client_1.ControlCommand[64],
+                        68: elero_usb_transmitter_client_1.ControlCommand[68],
+                    },
+                    write: true,
+                    read: true,
+                    def: 16,
+                    defAck: true,
+                    type: 'number',
+                },
+                native: {},
+            });
+            this.adapter.log.debug(`Create state info.`);
+            yield this.adapter.setObjectNotExistsAsync(`channel_${channel}.info`, {
+                type: 'state',
+                common: {
+                    name: 'Info',
+                    role: 'text',
+                    write: false,
+                    read: true,
+                    def: '',
+                    type: 'string',
+                },
+                native: {},
+            });
+            this.adapter.log.debug(`Create state open.`);
+            yield this.adapter.setObjectNotExistsAsync(`channel_${channel}.open`, {
+                type: 'state',
+                common: {
+                    name: 'Open',
+                    role: 'switch',
+                    read: true,
+                    write: true,
+                    def: false,
+                    type: 'boolean',
+                },
+                native: {},
+            });
+            this.adapter.log.debug(`Device with channel ${channel} created.`);
+        });
     }
     updateDeviceNames() {
         return __awaiter(this, void 0, void 0, function* () {
